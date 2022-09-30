8b60f1f6a366c395d09f55a02904670f4137992c
Bambam320 <igor.mestechkin@gmail.com>
2022-09-29 22:21:57 -0400

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    arr = self.split(/\.|\?|\!{2}/)
+    puts arr
+    puts arr.length
+    arr.length
   end
 end
